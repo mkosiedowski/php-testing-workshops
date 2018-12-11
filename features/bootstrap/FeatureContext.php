928c071ed27d491f40461398d0458dfd1d3db61f
Robert Burczak <robert.burczak@schibsted.pl>
2018-12-11 20:48:49 +0100

    Task 1

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,52 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Domain\Basket;
+use Domain\Item;
+use Domain\Price;
+use Assert\Assertion;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /** @var Basket */
+    private $basket;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Given I have empty basket
+     */
+    public function iHaveEmptyBasket()
+    {
+        $this->basket = new Basket();
+    }
+
+    /**
+     * @When I add item to this basket
+     */
+    public function iAddItemToThisBasket()
+    {
+        $item = new Item('some item', new Price(100, 'PLN'));
+        $this->basket->addItem($item);
+    }
+
+    /**
+     * @Then Basket should contain one item
+     */
+    public function basketShouldContainItem()
+    {
+        Assertion::count($this->basket->getItems(), 1);
+    }
+}
